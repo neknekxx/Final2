d2107d13d4f0d6862c15a62eaf97a5eeb4bd5dc6
neknekxx <126321245+neknekxx@users.noreply.github.com>
2025-04-04 23:26:02 +0800

    Update settings.py

--- periodic_table/settings.py
+++ periodic_table/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-t7+1vwf20d9*^(-h%zbtrvri31l-)y5y)kszq=$v0409wisrpn'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
